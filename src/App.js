80fe17dcda5f53745fd0e56241b291771a7d9f08
Elias <eliashelin98@gmail.com>
2022-11-09 17:57:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const GET_JOBS = gql`  
+  query GetCountries {
+    countries {
+      code,
+      name,
+      continent{name}
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data } = useQuery(GET_JOBS);
+  if (loading) return<p>Loading...</p>
+  else if (error) return <p>Error...</p>
+  else { 
+    return (
+      <div className="App">
+        <table>
+        <th>Code</th> <th>Country</th> <th>Continent</th>
+          <tbody> 
+            { data.countries.map((country, index) => 
+              <tr key={index}>
+                <td>{country.code}</td>
+                <td>{country.name}</td>
+                <td>{country.continent.name}</td>
+              </tr>            
+            )}
+          </tbody>
+        </table>
+      </div>    
+    );  
+  }
 }
-
 export default App;
